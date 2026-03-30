6d0d20daecf1e9ebacba1a9bad7a8a9425043dc1
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-03-30 17:15:32 +0530

    create jwt.ts

---
+++ frontend/lib/jwt.ts
@@ -0,0 +1,15 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+interface TokenPayload extends JwtPayload {
+  id: string;
+}
+
+export function signToken(payload: object) {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+}
+
+export function verifyToken(token: string): TokenPayload {
+  return jwt.verify(token, JWT_SECRET) as TokenPayload;
+}
