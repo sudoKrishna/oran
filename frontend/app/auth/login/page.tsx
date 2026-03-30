81efd3a8d898e22f5c9c6a37ce70cdfba5183f7f
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-03-30 20:18:15 +0530

    create auth pages

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-white to-gray-100">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ frontend/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "./SignupForm";
+
+export default function SignupPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-white to-gray-100">
+      <SignupForm />
+    </div>
+  );
+}
