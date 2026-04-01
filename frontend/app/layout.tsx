0b518138545b8b72dbdf0a01f6eed46e00f388e2
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-04-01 23:31:40 +0530

    wrap in provider laytout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 
 const geistSans = Geist({
@@ -29,9 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      
-        {children}
-        </body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
