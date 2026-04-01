5af89e0b7fb67e1e444597f7061188a1b2aa43f1
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-04-01 15:56:16 +0530

    removeing the old routes

--- frontend/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-
-export async function POST() {
-  const cookieStore = await cookies();
-
-  cookieStore.delete("token");
-
-  return Response.json({ message: "Logged out" });
-}
