c9116b9d776c473242292f6188ed8d4c8f9248f3
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-04-02 10:54:48 +0530

    change the projectId page.tsx imports

--- frontend/app/projects/[projectId]/page.tsx
+++ frontend/app/projects/[projectId]/page.tsx
@@ -1,4 +1,4 @@
-import VSCodeUI from '@/app/components/CodeEditor';   
+import VSCodeUI from '@/app/editor/components/VSCodeUI';   
 
 export default function ProjectPage({ params }: { params: { projectId: string } }) {
   return <VSCodeUI projectId={params.projectId} />;
